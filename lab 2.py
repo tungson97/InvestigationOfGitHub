d07ede38cdb7a0786d9b37af95e98f8cbeee031b
tungson97 <nguyentungson97@mail.ru>
2019-03-16 00:05:03 +0300

    Updating method of create user

--- lab 2.py
+++ lab 2.py
@@ -123,7 +123,7 @@ plt.xlabel("Температура в мае")
 plt.ylabel("Температура в июне")
 plt.legend()
 plt.show()
-
+##alsjndjkasndkjasndjkasndjknasjkn
 sigma2_eps = np.var(data[2]) * (1 - r_xy ** 2)
 print(sigma2_eps)
 sigma_eps = math.sqrt(sigma2_eps)
